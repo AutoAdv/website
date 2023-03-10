f9f12435e352d6d1525a9939a49a4b48808889cf
AutoAdv <127460662+AutoAdv@users.noreply.github.com>
2023-03-10 16:31:41 +0800

    Test Search Bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,35 @@ import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
 export default function RootLayout({children}) {
+
+  function searchFunction() {
+    var input, filter, ul, li, txtValue;
+    input = document.getElementById("mySearchBar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myList");
+    li = ul.getElementsByTagName("li");
+    for (var i = 0; i < li.length; i++) {
+      txtValue = li[i].textContent || li[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
   return (
     <html lang='en'>
       <head />
       <body>
+        <input type="text" id="mySearchBar" onkeyup="searchFunction()" placeholder="Search...">
+        <ul id="myList">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+          <li>Item 5</li>
+        </ul>
         <CacheProvider>
           <ChakraProvider>{children}</ChakraProvider>
         </CacheProvider>
